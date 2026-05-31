9f5827db0d90e2d429f77543a269feeb59a8b3f2
Priya-parthiban-coder <25cu0310011@student.hindustanuniv.ac.in>
2026-05-31 19:02:32 +0530

    Create hangmangame.py

---
+++ hangmangame.py
@@ -0,0 +1,32 @@
+import random
+
+def play_word_rescue():
+    words = ["PUZZLE", "ROCKET", "GARDEN", "WIZARD", "CASTLE"]
+    word = random.choice(words).upper()
+    guessed, wrong = [], 0
+    stages = [
+        "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n========="
+    ]
+
+    print("--- WORD RESCUE ---")
+    while wrong < 6:
+        print(stages[wrong])
+        print("Word: " + " ".join([l if l in guessed else "_" for l in word]))
+        guess = input("Guess a letter: ").upper()
+        if len(guess) == 1 and guess.isalpha() and guess not in guessed:
+            guessed.append(guess)
+            if guess not in word:
+                wrong += 1
+            if all(l in guessed for l in word):
+                print(f"Victory! The word was {word}")
+                return
+    print(stages[6] + f"\nGame Over! The word was {word}")
+
+if __name__ == "__main__":
+    play_word_rescue()
