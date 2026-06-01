6419eddf23968fe067bd27381a4a9b8321d0d218
Priya-parthiban-coder <25cu0310011@student.hindustanuniv.ac.in>
2026-06-01 22:04:57 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,23 @@
+def chatbot():
+    print("Chatbot: Hello! Type something to start chatting. Type 'exit' to quit.")
+    while True:
+        user_input = input("You: ").strip().lower()
+        if user_input == "exit":
+            print("Chatbot: Goodbye!")
+            break
+        elif user_input == "hi":
+            print("Chatbot: Hello!")
+        elif user_input == "hello":
+            print("Chatbot: Hi!")
+        elif user_input == "how are you":
+            print("Chatbot: I'm fine, thanks! how are you ? ")
+        elif user_input == "i am fine. thank you":
+            print("Chatbot: that's great")    
+        elif user_input == "bye":
+            print("Chatbot: Goodbye!")
+            break
+        else:
+            print("Chatbot: Sorry, I don't understand that.")
+
+if __name__ == "__main__":
+    chatbot()
